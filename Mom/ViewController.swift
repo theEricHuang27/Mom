ba3cda0251fcb22f3c180c6da3f24420bb228ba0
Aidan Barr Bono (student LM) <s012506@lm-20s012506.lmsd.org>
2019-01-03 11:56:39 -0500

    Initial Commit

---
+++ Mom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mom
+//
+//  Created by Aidan Barr Bono (student LM) on 1/3/19.
+//  Copyright © 2019 Duck Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MomTests/MomTests.swift
@@ -0,0 +1,34 @@
+//
+//  MomTests.swift
+//  MomTests
+//
+//  Created by Aidan Barr Bono (student LM) on 1/3/19.
+//  Copyright © 2019 Duck Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Mom
+
+class MomTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
