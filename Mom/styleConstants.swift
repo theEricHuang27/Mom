fd3bc17fe4fbc96f6e5d6e24f383eca02b4e558c
Chang-Chi Huang (student LM) <s013455@lm-20s013455.lmsd.org>
2019-01-11 08:21:22 -0500

    restart everything since we messed up

---
+++ Mom/styleConstants.swift
@@ -0,0 +1,24 @@
+//
+//  styleConstants.swift
+//  Mom
+//
+//  Created by Chang-Chi Huang (student LM) on 1/4/19.
+//  Copyright © 2019 Duck Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+}
+
+extension UIFont {
+    
+}
+
+class style {
+    static let buttonColor = UIColor(red: 0, green: 0.2, blue: 0.9, alpha: 1)
+    static let backgroundColor = UIColor(red: 0, green: 0.2, blue: 0.9, alpha: 1)
+}
+
