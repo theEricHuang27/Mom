b08716c2256ba055e10d489fa976214eac28fec7
Aidan Barr Bono (student LM) <s012506@lm-20s012506.lmsd.org>
2019-03-18 13:13:00 -0400

    i got it with date components

--- Mom/Notifications.swift
+++ Mom/Notifications.swift
@@ -12,21 +12,9 @@ import UserNotifications
 class Notifications : NSObject {
     
     static var snoozeTime = 5
-    static var reminderTime = 5
+//    static var reminderTime : [Int]? = [0, 0, 0]
+    static var reminderDateComponents : DateComponents? = DateComponents(year: 0, month: 0, day: 0, hour: 0, minute: 0, second: 5)
     
-    
-//    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-//        completionHandler([.alert, .sound])
-//    }
-//
-//    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-//        // code when an ation is chosen
-//        switch response.actionIdentifier {
-//        case "SNOOZE": Notifications.generateSnoozeNotificationFrom(response.notification.request)
-//        case "VIEW": break
-//        default: break
-//        }
-//    }
     
     static func generateNotificationFrom(_ event : Event) {
         let content = UNMutableNotificationContent()
@@ -39,12 +27,20 @@ class Notifications : NSObject {
         
         
         let calendar = Calendar.current
-        var dateComponents = calendar.dateComponents([.second, .minute, .hour, .day, .month, .year], from: event.date)
-        dateComponents.second = dateComponents.second! + 15
+        var dateComponents = DateComponents()
+        
+        if let reminderComponents = reminderDateComponents {
+                
+                dateComponents = calendar.dateComponents([.second, .minute, .hour, .day, .month, .year], from: event.date)
+                dateComponents.day = dateComponents.day! - (reminderComponents.day ?? 0)
+                dateComponents.hour = dateComponents.hour! - (reminderComponents.hour ?? 0)
+                dateComponents.minute = dateComponents.minute! - (reminderComponents.minute ?? 0)
+                dateComponents.second = dateComponents.second! - (reminderComponents.second ?? 0)
+                
+        }
         
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
-//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
         
         let request = UNNotificationRequest(identifier: "ALERT", content: content, trigger: trigger)
         
