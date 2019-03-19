261442f7f584306128621f7494fe8abca05dc787
Connor Stange <s025780@lm-20s025780.lmsd.org>
2019-03-19 14:03:10 -0400

    Updated event and next view controllers and event class

--- Mom/NextViewController.swift
+++ Mom/NextViewController.swift
@@ -4,19 +4,30 @@
 //
 //  Created by Connor Stange (student LM) on 1/29/19.
 //  Copyright © 2019 Duck Inc. All rights reserved.
-//
+// display
 
 import UIKit
 
 class NextViewController: UIViewController {
-
+    
     var events: [Event] = []
     @IBOutlet weak var DateLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         DateLabel.text = dateString
+        if let e = defaults.array(forKey: dateString){
+            events = e as! [Event]
+        }
+//        events = UserDefaults.standard.array(forKey: dateString)
     }
     @IBAction func NewEvent(_ sender: Event) {
-
+//        events.append(getEvent())
     }
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        <#code#>
+//    }
 }
---
+++ Pods/Target Support Files/Pods-Mom/Pods-Mom-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mom : NSObject
+@end
+@implementation PodsDummy_Pods_Mom
+@end
