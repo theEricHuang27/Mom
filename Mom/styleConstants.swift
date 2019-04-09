4ec6b23879ba4e8f111d394e977b501f1b1c40cb
Aidan Barr Bono (student LM) <s012506@lm-20s012506.lmsd.org>
2019-04-09 15:26:28 -0400

    Updated colors

--- Mom/styleConstants.swift
+++ Mom/styleConstants.swift
@@ -11,6 +11,17 @@ import UIKit
 
 extension UIColor {
     
+    static var myPurple = UIColor(red: 0.42, green: 0.06, blue: 0.73, alpha: 1.0)
+    static var myMagenta = UIColor(red: 0.58, green: 0.15, blue: 0.56, alpha: 1.0)
+    static var myDeepPink = UIColor(red: 1.00, green: 0.28, blue: 0.76, alpha: 1.0)
+    static var myPink = UIColor(red: 1.00, green: 0.75, blue: 0.94, alpha: 1.0)
+    static var myLightPink = UIColor(red: 1.00, green: 0.88, blue: 0.98, alpha: 1.0)
+    static var myDeepGrey = UIColor(red: 0.30, green: 0.30, blue: 0.30, alpha: 1.0)
+    static var myGrey = UIColor(red: 0.80, green: 0.80, blue: 0.80, alpha: 1.0)
+    static var myLightGrey = UIColor(red: 0.90, green: 0.90, blue: 0.90, alpha: 1.0)
+    static var myYellow = UIColor(red: 1.00, green: 0.89, blue: 0.37, alpha: 1.0)
+    static var myBlue = UIColor(red: 0.00, green: 0.55, blue: 0.78, alpha: 1.0)
+    
 }
 
 extension UIFont {
